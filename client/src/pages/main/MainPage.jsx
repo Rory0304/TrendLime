ffba95fb3bcaec19c7c27e4911d06382e734c9db
Eunsoo Sa <mitty0304@naver.com>
2021-11-23 18:29:56 +0900

    feat(#17): 메인 페이지 기본 컴포넌트 구성

--- client/src/pages/main/MainPage.jsx
+++ client/src/pages/main/MainPage.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import route from '../../routers/routeConstants';
+
+import ContentBlock from '../../components/ContentBlock/index';
+import Container from '../../components/Container/index';
+import ScrollToTop from '../../common/ScrollToTop/index';
 
 function MainPage() {
     return (
-        <div>
-            <Link to={route.SURVEY}>오늘의 곡 추천</Link>
-        </div>
+        <Container>
+            <ScrollToTop />
+            <ContentBlock
+                type="right"
+                title="인트로"
+                content="인트로"
+                button={{ text: '오늘의 곡 추천', link: route.SURVEY }}
+            />
+            <ContentBlock type="left" title="서비스1" content="서비스1" />
+            <ContentBlock type="right" title="서비스2" content="서비스2" />
+            <ContentBlock type="left" title="서비스3" content="서비스3" />
+        </Container>
     );
 }
 
