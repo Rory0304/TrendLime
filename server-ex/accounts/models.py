5ea23ea98049d8d38ddbb7eadcd7eda27d438b33
[2기-2반] 이승환 <shakur25@naver.com>
2021-11-26 20:12:14 +0900

    Chore:  필요없는 파일 삭제 및 정리

--- server-ex/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-class User(AbstractUser): 
-  #새로운 인자 추가가능
-  #예시
-  #jobs=models.CharField(max_length=10)
-  pass
