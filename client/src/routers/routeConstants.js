cdd0ab68f539d18e70f20f87cc8428471fbebf56
Eunsoo Sa <mitty0304@naver.com>
2021-11-27 03:33:48 +0900

    feat: 어바웃 페이지, 검색 페이지 라우터 변수 추가

--- client/src/routers/routeConstants.js
+++ client/src/routers/routeConstants.js
@@ -1,9 +1,13 @@
 const route = {
     MAIN: '/',
     LOGIN: '/login',
+    SIGNUP: '/signup',
     NOTFOUND: '/notfound',
     SURVEY: '/survey',
     SURVEYRESULT: '/survey/result',
+    ABOUT: '/about',
+    SEARCH: '/search',
+    DETAIL:'/search/song/'
 };
 
 export default route;
