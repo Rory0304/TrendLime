e76f36e5dd5d1a47238ffa0d49c7962eca36954b
[2기-2반] 이승환 <shakur25@naver.com>
2021-11-26 12:38:59 +0900

    feat(search): 노래 검색앱 등록

--- server/Config/urls.py
+++ server/Config/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from search.views import UserViewSet, SongViewSet
+from search.views import UserViewSet, SongViewSet, search
+
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'songs', SongViewSet)
+router.register(r'search', search)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
