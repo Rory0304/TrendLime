57e4efef48e26aff6907e13169cdd43110275673
[2기-2반] 이승환 <shakur25@naver.com>
2021-11-27 09:50:10 +0900

    fix: 불필요한 코드 혹은 주석 삭제

--- server/search/views.py
+++ server/search/views.py
@@ -14,59 +14,33 @@ from search.serializers import UserSerializer, SongSerializer
 
 def search(request):
   search_word = json.loads(request.body)['q']
-  print('1',search_word)
-  # search_word를 확인 하고 검색어 특정할 것
+
+  # 다른 옵션을 추가로 검색을 할 경우
+  # selected_tag = json.loads(request.body)['tag']
+  # tag_content = json.loads(request.body)['tag_content']
+
+  # 아래의 queryset_list와 and 혹은 or로 붙여서 검색
+  # Song.objects.filter(f'{selected_tag}'__icontains = f'{tag_content}')
+  
   result_list=[]
   queryset_list = Song.objects.filter(song_name__icontains = f'{search_word}')
-  # queryset_list = Song.objects.filter(song_name= search_word)
-  # # query_result = db.session.query(Song).filter(Song.song_name.like(f"%{search_word}%"))
-  print('queryset_list',queryset_list)
 
-  # 1 안
   if queryset_list.exists():
     for queryset in queryset_list:
-      # print('queryset',queryset.song_name)
       result_list.append({
-          'song_id' : queryset.song_id,
-          'song_name' : queryset.song_name,
-          'artist' : queryset.artist,
-          'album' : queryset.album,
-          'Like_Count' : queryset.Like_Count,
-          'Lyric' : queryset.Lyric,
-          'cover_url' : queryset.cover_url,
-          'tags' : queryset.tags,
-          'year' : queryset.year,
-        })
-      # for data in queryset:
-      #   print('data',data.song_id)
-        # result_list.append({
-        #   'song_id' : queryset[0],
-        #   'song_name' : queryset[1],
-        #   'artist' : queryset[2],
-        #   'album' : queryset[3],
-        #   'Like_Count' : queryset[4],
-        #   'Lyric' : queryset[5],
-        #   'cover_url' : queryset[6],
-        #   'tags' : queryset[7],
-        #   'year' : queryset[8]
-        # })
+        'song_id' : queryset.song_id,
+        'song_name' : queryset.song_name,
+        'artist' : queryset.artist,
+        'album' : queryset.album,
+        'Like_Count' : queryset.Like_Count,
+        'Lyric' : queryset.Lyric,
+        'cover_url' : queryset.cover_url,
+        'tags' : queryset.tags,
+        'year' : queryset.year,
+      })
   else:
-    print('2')
     result_list.append(None) 
-
   context = {"result" : result_list}
-
-  # 2 안
-  # cursor = connection.cursor()
-  # print('cursor',cursor)
-  # query_stmt = ('db.tb_song.find({"song_name":/{}/})'.format(search_word))
-
-  # print('query_stmt',query_stmt)
-  # query_result = cursor.execute(query_stmt)
-  # connection.commit()
-  # connection.close()
-  # print('query_result',query_result)
-
   return JsonResponse(context, status = 200)
 
 
