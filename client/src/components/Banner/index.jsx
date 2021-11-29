76600c86ad876c4c14a7e38239319fe8375103b8
Eunsoo Sa <mitty0304@naver.com>
2021-11-30 05:21:34 +0900

    feat: 전역용 배너 컴포넌트 생성

---
+++ client/src/components/Banner/index.jsx
@@ -0,0 +1,16 @@
+import Button from '../../common/Button/index';
+import { Styled } from './styles';
+import lime from '../../assets/lime.png';
+
+const Banner = ({ title, subtitle, button }) => {
+    return (
+        <Styled.BannerWrapper>
+            <Styled.BannerTitle>
+                {title} <br /> {subtitle}
+            </Styled.BannerTitle>
+            <Button link={button.link} text={button.text} />
+        </Styled.BannerWrapper>
+    );
+};
+
+export default Banner;
