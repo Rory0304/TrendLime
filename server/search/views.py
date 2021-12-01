52ccf0091af84f39e987449ba5c9ae31cf384662
[2기-2반] 이승환 <shakur25@naver.com>
2021-12-01 18:25:15 +0900

    feat: 카테고리 선택에 따른 데이터 전송 api 수정

--- server/search/views.py
+++ server/search/views.py
@@ -13,52 +13,59 @@ from search.serializers import UserSerializer, SongSerializer, TagSerializer, So
 # Create your views here.
 
 def search(request):
+  result_list=[]
   search_word = json.loads(request.body)['q']
   # 다른 옵션을 추가로 검색을 할 경우
   selected_tag = json.loads(request.body)['category']
   tag_content = json.loads(request.body)['tag']
 
-  if selected_tag == "트렌드":
-    queryset_list1 = Top11.objects.filter(year__icontains = f'{tag}') 
-  elif not selected_tag:
+  if not selected_tag:
     selected_tag = "tags"
-    queryset_list1 = Song.objects.filter(song_name__icontains = f'{search_word}') 
-    queryset_list2 = Song.objects.filter(**{fieldname_icontains : tag_content})
-  else : 
-    fieldname_icontains = selected_tag + '__icontains'
-
-    queryset_list1 = Song.objects.filter(song_name__icontains = f'{search_word}') 
-    queryset_list2 = Song.objects.filter(**{fieldname_icontains : tag_content})
   
-  # 아래의 queryset_list와 and 혹은 or로 붙여서 검색
-  # Song.objects.filter(f'{selected_tag}'__icontains = f'{tag_content}')
+  fieldname_icontains = selected_tag + '__icontains'
 
-  result_list=[]
-  queryset_list1 = Song.objects.filter(song_name__icontains = f'{search_word}') 
-  queryset_list2 = Song.objects.filter(**{fieldname_icontains : tag_content})
-  # queryset_list3 = Song_without_year.objects.filter(song_name__icontains = f'{search_word}') 
-  # queryset_list4 = Song_without_year.objects.filter(**{fieldname_icontains : tag_content})
+  if selected_tag == "트렌드":
+    queryset_list = Top11.objects.filter(year__icontains = f'{tag_content}') 
 
-  if queryset_list2:
-    queryset_list = (queryset_list1 & queryset_list2).order_by('-Like_Count')#, 'year')
-  else : 
-    queryset_list = queryset_list1.order_by('-Like_Count')#, 'year')
-  print(queryset_list.count())
-  if queryset_list.exists():
     for queryset in queryset_list:
       result_list.append({
-        'song_id' : queryset.song_id,
-        'song_name' : queryset.song_name,
-        'artist' : queryset.artist,
-        'album' : queryset.album,
-        'Like_Count' : queryset.Like_Count,
-        'Lyric' : queryset.Lyric,
-        'cover_url' : queryset.cover_url,
-        'tags' : queryset.tags,
-        'year' : queryset.year,
+        'word' : queryset.word,
+        'freq' : queryset.freq,
+        'year' : queryset.year
       })
   else:
-    result_list.append(None) 
+    queryset_list1 = Song.objects.filter(song_name__icontains = f'{search_word}') 
+    queryset_list2 = Song.objects.filter(**{fieldname_icontains : tag_content})
+    
+    # 아래의 queryset_list와 and 혹은 or로 붙여서 검색
+    # Song.objects.filter(f'{selected_tag}'__icontains = f'{tag_content}')
+
+    # queryset_list1 = Song.objects.filter(song_name__icontains = f'{search_word}') 
+    # queryset_list2 = Song.objects.filter(**{fieldname_icontains : tag_content})
+    # queryset_list3 = Song_without_year.objects.filter(song_name__icontains = f'{search_word}') 
+    # queryset_list4 = Song_without_year.objects.filter(**{fieldname_icontains : tag_content})
+
+    if queryset_list2:
+      queryset_list = (queryset_list1 & queryset_list2).order_by('-Like_Count')#, 'year')
+    else : 
+      queryset_list = queryset_list1.order_by('-Like_Count')#, 'year')
+    print('result_count', queryset_list.count())
+
+    if queryset_list.exists():
+      for queryset in queryset_list:
+        result_list.append({
+          'song_id' : queryset.song_id,
+          'song_name' : queryset.song_name,
+          'artist' : queryset.artist,
+          'album' : queryset.album,
+          'Like_Count' : queryset.Like_Count,
+          'Lyric' : queryset.Lyric,
+          'cover_url' : queryset.cover_url,
+          'tags' : queryset.tags,
+          'year' : queryset.year,
+        })
+    else:
+      result_list.append(None) 
   context = {"result" : result_list}
   return JsonResponse(context, status = 200)
 
