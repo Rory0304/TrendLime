b270a1f3c4352a8e5a2a20dbad81e7fba6c6dedc
Eunsoo Sa <mitty0304@naver.com>
2021-12-05 23:54:31 +0900

    feat: 워드 클라우드 영역 추가

--- client/src/components/WordCloud/index.jsx
+++ client/src/components/WordCloud/index.jsx
@@ -1,28 +1,36 @@
-/* TODO : 현재 워드 클라우드가 리액트와 버전 충돌이 있음. */
-// import React from 'react';
-// import ReactWordcloud from 'react-wordcloud';
+import React, { useCallback } from 'react';
+import WordCloud from 'react-d3-cloud';
+import { Styled } from './styles.js';
 
-// function Wordcloud() {
-//     const words = [
-//         {
-//             text: 'told',
-//             value: 64,
-//         },
-//         {
-//             text: 'mistake',
-//             value: 11,
-//         },
-//         {
-//             text: 'thought',
-//             value: 16,
-//         },
-//         {
-//             text: 'bad',
-//             value: 17,
-//         },
-//     ];
+function Wordcloud() {
+    const data = [
+        { text: '안녕', value: 1000 },
+        { text: '안녕', value: 200 },
+        { text: '안녕', value: 800 },
+        { text: '안녕', value: 1000000 },
+        { text: '안녕', value: 10 },
+    ];
 
-//     return <ReactWordcloud words={words} />;
-// }
+    const sum = data.reduce((a, b) => a.value + b.value, 0);
+    const avg = sum / data.length || 0;
+    const fontSize = useCallback((word) => Math.log2(word.value) * 3, []);
+    const fontWeight = useCallback((word) => (word.value > avg ? 'bold' : 'normal'), []);
+    // const fill = useCallback((word) => (word.value > avg ? '#00cc66' : 'gray'), []);
 
-// export default Wordcloud;
+    return (
+        <Styled.WordCloudWrapper>
+            <WordCloud
+                height={200}
+                data={data}
+                fontSize={fontSize}
+                fontWeight={fontWeight}
+                // fill={fill}
+                rotate={0}
+                padding={10}
+                font="Noto Sans KR"
+            />
+        </Styled.WordCloudWrapper>
+    );
+}
+
+export default Wordcloud;
