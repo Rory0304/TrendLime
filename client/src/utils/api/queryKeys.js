f0eab41a4a614a6d3a1092bf09321247505dde92
Eunsoo Sa <mitty0304@naver.com>
2021-12-06 01:45:05 +0900

    fix: build 시, 프록시 설정 오류 해결

--- client/src/utils/api/queryKeys.js
+++ client/src/utils/api/queryKeys.js
@@ -1,3 +1,3 @@
-export const fetchCategoryKey = '/api/tags';
-export const fetchSearchKey = '/api/search';
-export const featchTotalSearchKey = '/api/search/total';
+export const fetchCategoryKey = `${process.env.REACT_APP_DB_HOST}/api/tags`;
+export const fetchSearchKey = `${process.env.REACT_APP_DB_HOST}/api/search`;
+export const featchTotalSearchKey = `${process.env.REACT_APP_DB_HOST}/api/search/total`;
