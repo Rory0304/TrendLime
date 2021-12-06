dfbed968f0a192db45371eda4e0f5056e0c6b2be
Eunsoo Sa <mitty0304@naver.com>
2021-12-07 04:22:33 +0900

    feat: 곡 정보 api 호출
    Former-commit-id: d774996d41fb0187d6f260417a79e03f5083be3a

--- client/src/pages/songInfo/SongInfoPage.jsx
+++ client/src/pages/songInfo/SongInfoPage.jsx
@@ -5,10 +5,10 @@ import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import { css, jsx } from '@emotion/react';
 import { Styled } from './styles';
 
-import { featchSongInfoKey, featchSongEmotionKey } from '../../utils/api/queryKeys';
+import { featchSongInfoKey } from '../../utils/api/queryKeys';
 import { useQueryFetch } from '../../utils/hooks/useQueryFetch';
-import tempData from './tempData';
 
+import TopicSection from './TopicSection';
 import EmotionSection from './EmotionSection';
 import RecommendSongSection from './RecommendSongSection';
 
@@ -28,25 +28,22 @@ function SongInfoContents() {
 
     const [lyricsOpen, setLyricscOpen] = useState(false);
 
-    // const {
-    //     isLoading,
-    //     error,
-    //     data: album,
-    // } = useQuery([featchSongInfoKey, { song_id: songId }], useQueryFetch, {
-    //     initialData: [],
-    //     refetchOnWindowFocus: false,
-    //     refetchOnmount: false,
-    //     refetchOnReconnect: false,
-    //     retry: false,
-    // });
-
     const {
         isLoading,
-        data: album,
         error,
-    } = useQuery('categoryData', () => fetch('').then((res) => tempData));
+        data: album,
+    } = useQuery([featchSongInfoKey, { song_id: songId }], useQueryFetch, {
+        initialData: { Lyric: '', cover_url: '', song_name: '' },
+        refetchOnWindowFocus: false,
+        refetchOnmount: false,
+        refetchOnReconnect: false,
+        retry: false,
+    });
 
-    const slitedLyrics = useMemo(() => (album?.lyrics ? album.lyrics.split('  ') : []), [album]);
+    const slitedLyrics = useMemo(
+        () => (album?.Lyric ? album.Lyric.split('  ').filter((sentence) => sentence !== '') : []),
+        [album],
+    );
 
     if (isLoading) return <div>'Loading...'</div>;
 
@@ -54,7 +51,6 @@ function SongInfoContents() {
 
     return (
         <div>
-            {isLoading}
             <Styled.SummaryInfo>
                 <Styled.SummaryInfoWrapper>
                     <Styled.AlbumCover>
@@ -68,45 +64,27 @@ function SongInfoContents() {
                 <div css={BackgroundWrapper({ cover_url: album.cover_url })}></div>
             </Styled.SummaryInfo>
             <Styled.MainInfo>
-                <Styled.UpperInfo>
-                    <Styled.Lyrics>
-                        <h3>가사정보</h3>
-                        <Styled.LyricsWrapper open={lyricsOpen}>
-                            <p>
-                                {slitedLyrics.map((sentence) => (
-                                    <>
-                                        {sentence}
-                                        <br />
-                                    </>
-                                ))}
-                            </p>
-                        </Styled.LyricsWrapper>
-                        <button onClick={() => setLyricscOpen(!lyricsOpen)}>
-                            {lyricsOpen ? '접기' : '펼치기'}
-                        </button>
-                    </Styled.Lyrics>
-                    <Styled.Topic>
-                        <Styled.TopicWrodCloud>
-                            <h3>가사의 주요 토픽</h3>
-                            <div></div>
-                        </Styled.TopicWrodCloud>
-                        <Styled.TopicSongs>
-                            <h3>‘핵심 단어’에 대한 유사한 곡</h3>
-                            <div>
-                                <ul>
-                                    <li>유사곡1</li>
-                                    <li>유사곡2</li>
-                                    <li>유사곡3</li>
-                                    <li>유사곡4</li>
-                                </ul>
-                            </div>
-                        </Styled.TopicSongs>
-                        <EmotionSection songId={songId} />
-                    </Styled.Topic>
-                </Styled.UpperInfo>
-                <Styled.BottomInfo>
+                <Styled.LeftInfo>
+                    <TopicSection songId={songId} />
+                    <EmotionSection songId={songId} />
                     <RecommendSongSection songId={songId} />
-                </Styled.BottomInfo>
+                </Styled.LeftInfo>
+                <Styled.RightInfo>
+                    <h3>가사정보</h3>
+                    <Styled.LyricsWrapper open={lyricsOpen}>
+                        <p>
+                            {slitedLyrics.map((sentence) => (
+                                <>
+                                    {sentence}
+                                    <br />
+                                </>
+                            ))}
+                        </p>
+                    </Styled.LyricsWrapper>
+                    <button onClick={() => setLyricscOpen(!lyricsOpen)}>
+                        {lyricsOpen ? '접기' : '펼치기'}
+                    </button>
+                </Styled.RightInfo>
             </Styled.MainInfo>
         </div>
     );
