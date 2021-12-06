5d304e047ae5c90bf8b8591981d3b7d9041f3ac5
Eunsoo Sa <mitty0304@naver.com>
2021-12-07 02:55:06 +0900

    feat: 트렌드 카테고리와 그 외 카테고리에 해당하는 컴포넌트 분리
    Former-commit-id: 444fc1210ff7317d25cebf3e2a10a77600967bf0

---
+++ client/src/pages/search/Contents/GeneralContents.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import Wordcloud from '../../../components/WordCloud/index';
+import BarChart from '../../../components/BarChart/index';
+import { Styled } from './styles';
+
+function GeneralContents({ isFetching, data }) {
+    const top30Data = data?.length === 0 ? [] : data.words_and_freq.slice(0, 30);
+    const top10Data = data?.length === 0 ? [] : data.words_and_freq.slice(0, 10);
+
+    return (
+        <>
+            <Styled.SubContentsWrapper>
+                <Styled.SubTitle>가사 속 표현 분석</Styled.SubTitle>
+                <Styled.SubContentArea>
+                    {isFetching ? (
+                        <div>loading...</div>
+                    ) : data?.length === 0 ? (
+                        <div>데이터가 없습니다. </div>
+                    ) : (
+                        <Wordcloud data={top30Data} />
+                    )}
+                </Styled.SubContentArea>
+            </Styled.SubContentsWrapper>
+            <Styled.SubContentsWrapper>
+                <Styled.SubTitle>노래 가사 TOP10</Styled.SubTitle>
+                <Styled.SubContentArea>
+                    {isFetching ? (
+                        <div>loading...</div>
+                    ) : data?.length === 0 ? (
+                        <div>데이터가 없습니다. </div>
+                    ) : (
+                        <BarChart data={top10Data} />
+                    )}
+                </Styled.SubContentArea>
+            </Styled.SubContentsWrapper>
+        </>
+    );
+}
+
+export default GeneralContents;
