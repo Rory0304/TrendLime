388552998e86b547a0eccfa163e61cbbcc91ad10
Eunsoo Sa <mitty0304@naver.com>
2021-12-06 13:38:40 +0900

    feat: 배포 설정에 따른 엔드포인트 변경

--- client/src/utils/api/queryKeys.js
+++ client/src/utils/api/queryKeys.js
@@ -1,3 +1,7 @@
 export const fetchCategoryKey = `${process.env.REACT_APP_DB_HOST}/api/tags`;
 export const fetchSearchKey = `${process.env.REACT_APP_DB_HOST}/api/search`;
 export const featchTotalSearchKey = `${process.env.REACT_APP_DB_HOST}/api/search/total`;
+export const featchSongInfoKey = `${process.env.REACT_APP_DB_HOST}/api/song/`;
+export const featchSongEmotionKey = `${process.env.REACT_APP_DB_HOST}/api/emotion/`;
+export const featchRecommendSongKey = `${process.env.REACT_APP_DB_HOST}/api/recommend/`;
+export const featchSongTopicKey = `${process.env.REACT_APP_DB_HOST}/api/topic/`;
