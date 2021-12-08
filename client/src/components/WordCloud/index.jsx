2056e9374640ef972c2ef9226aee8d646c790532
Eunsoo Sa <mitty0304@naver.com>
2021-12-08 15:35:55 +0900

    style: 폰트 사이즈 조절
    Former-commit-id: 74256113de33ae4eb2f74366da0f9836ad7b2540

--- client/src/components/WordCloud/index.jsx
+++ client/src/components/WordCloud/index.jsx
@@ -2,20 +2,21 @@ import React, { useCallback } from 'react';
 import WordCloud from 'react-d3-cloud';
 import { Styled } from './styles.js';
 
-function Wordcloud({ data }) {
+function Wordcloud({ data, height = 200, width = 500, fontsize = 1 }) {
     /* TODO : 데이터가 너무 많음!! 워드 클라우드에 표현해줄 데이터 length 지정하기! */
     const FilteredData = data.map((d) => ({ text: d.word, value: parseInt(d.freq) }));
 
     const sum = data.reduce((a, b) => a.value + b.value, 0);
     const avg = sum / data.length || 0;
-    const fontSize = useCallback((word) => Math.log2(word.value) * 3, []);
+    const fontSize = useCallback((word) => Math.log2(word.value * fontsize) * 3, []);
     const fontWeight = useCallback((word) => (word.value > avg ? 'bold' : 'normal'), []);
     // const fill = useCallback((word) => (word.value > avg ? '#00cc66' : 'gray'), []);
 
     return (
         <Styled.WordCloudWrapper>
             <WordCloud
-                height={200}
+                width={width}
+                height={height}
                 data={FilteredData}
                 fontSize={fontSize}
                 fontWeight={fontWeight}
