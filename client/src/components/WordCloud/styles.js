551eb04f5b9cd8b6a1f79ab2d6a19c8213a9eb3c
Eunsoo Sa <mitty0304@naver.com>
2021-12-10 04:28:21 +0900

    style: 워드 클라우드 스타일 수정
    Former-commit-id: a7e67b08d6bac4a4297d60ce99ad965409c99f90

--- client/src/components/WordCloud/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-export const Styled = {};
-
-Styled.WordCloudWrapper = styled.div`
-    background-color: #f5f8fc;
-    border: 1px solid #cecece;
-    border-radius: 8px;
-`;
