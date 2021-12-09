c5a99b29181d37735c3561f14d108cd5b599796c
Eunsoo Sa <mitty0304@naver.com>
2021-12-10 04:26:00 +0900

    style: 워드 클라우드 스타일 수정
    Former-commit-id: 07ca5a443e1604a624634e7ee67dcd42316ce808

--- client/src/components/WordCloud/index.jsx
+++ client/src/components/WordCloud/index.jsx
@@ -1,32 +1,52 @@
 import React, { useCallback } from 'react';
+import { css, jsx } from '@emotion/react';
 import WordCloud from 'react-d3-cloud';
-import { Styled } from './styles.js';
+import { scaleLinear, scalePow } from 'd3-scale';
 
-function Wordcloud({ data, height = 200, width = 500, fontsize = 1 }) {
-    /* TODO : 데이터가 너무 많음!! 워드 클라우드에 표현해줄 데이터 length 지정하기! */
+function Wordcloud({ data, height = 200, width = 600, fontsize = 15, fontValue = 8 }) {
     const FilteredData = data.map((d) => ({ text: d.word, value: parseInt(d.freq) }));
+    console.log(FilteredData);
+    const FilteredValue = FilteredData.map((fd) => fd.value);
 
-    const sum = data.reduce((a, b) => a.value + b.value, 0);
-    const avg = sum / data.length || 0;
-    const fontSize = useCallback((word) => Math.log2(word.value * fontsize) * 3, []);
+    let sum = FilteredData.reduce((a, b) => a + b.value, 0);
+    let avg = sum / FilteredData.length;
+    let maxValue = Math.max(...FilteredValue);
+    let minValue = Math.min(...FilteredValue);
+
+    let color = scaleLinear().domain([minValue, maxValue]).range(['#ebe86a', '#4bc2c4', '#03793e']);
+
+    const fontSize = useCallback((word) => {
+        const size = Math.log(word.value * fontsize) * fontValue;
+        return size;
+    }, []);
     const fontWeight = useCallback((word) => (word.value > avg ? 'bold' : 'normal'), []);
-    // const fill = useCallback((word) => (word.value > avg ? '#00cc66' : 'gray'), []);
+    const fill = useCallback(function (word) {
+        return color(word.value);
+    }, []);
 
     return (
-        <Styled.WordCloudWrapper>
+        <div css={WordCloudWrapper({ width: width, height: height })}>
             <WordCloud
                 width={width}
                 height={height}
                 data={FilteredData}
                 fontSize={fontSize}
                 fontWeight={fontWeight}
-                // fill={fill}
+                fill={fill}
                 rotate={0}
                 padding={10}
                 font="Noto Sans KR"
             />
-        </Styled.WordCloudWrapper>
+        </div>
     );
 }
+
+const WordCloudWrapper = ({ width, height }) => css`
+    background-color: #f5f8fc;
+    border: 1px solid #cecece;
+    border-radius: 8px;
+    width: ${width}px;
+    height: ${height}px;
+`;
 
 export default Wordcloud;
