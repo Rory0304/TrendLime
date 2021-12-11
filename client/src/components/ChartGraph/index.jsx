20358954e318fa131dedaf14a53d11ddd576d274
Eunsoo Sa <mitty0304@naver.com>
2021-12-12 03:34:20 +0900

    chore: 필요없는 파일 삭제
    Former-commit-id: c2322810cbf88786c607b0271330e7834e9a29d8

--- client/src/components/ChartGraph/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Styled } from './styles';
-
-function ChartGraph({ songs }) {
-    return <Styled.ChartGraphWrapper>{songs}</Styled.ChartGraphWrapper>;
-}
-
-export default ChartGraph;
