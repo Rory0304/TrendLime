4dbaa2cc38e5f5665d361debe26bf5935a8eef93
Eunsoo Sa <mitty0304@naver.com>
2021-12-21 16:33:11 +0900

    chore: useQueryFetch 함수명 변경

--- client/src/utils/api/queryFetch.js
+++ client/src/utils/api/queryFetch.js
@@ -4,10 +4,12 @@ const apiClient = axios.create({
     baseURL: process.env.REACT_APP_DB_HOST,
 });
 
-export const useQueryFetch = async ({ queryKey }) => {
+const queryFetch = async ({ queryKey }) => {
     const [key, params = {}] = queryKey;
     const { data } = await apiClient.get(key, {
         params: params,
     });
     return data;
 };
+
+export default queryFetch;
