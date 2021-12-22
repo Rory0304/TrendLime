e670791372b129d3b78a197783dd6d1bd380f1d6
Eunsoo Sa <mitty0304@naver.com>
2021-12-22 17:23:00 +0900

    chore: 바 차트 색상 배열 한 줄로 수정, 전역으로 사용

--- client/src/components/BarChart/index.jsx
+++ client/src/components/BarChart/index.jsx
@@ -3,23 +3,12 @@ import { css, jsx } from '@emotion/react';
 import Chart from 'chart.js/auto';
 import { Bar } from 'react-chartjs-2';
 
+const BACKGROUNDCOLOR = Array(10).fill('#00DD00', 0, 3).fill('#979797', 3, 10);
+const BORDERCOLOR = Array(10).fill('#00DD00', 0, 3).fill('#97979752', 3, 10);
+
 function BarChart({ data, width = '100%', height = '100%' }) {
     const values = data[0] === null ? [] : data.map((d) => parseFloat(d.freq));
     const labels = data[0] === null ? [] : data.map((d) => d.word);
-
-    const backgroundColor = [];
-    const borderColor = [];
-
-    /* TODO: 한 줄로 바꾸기 */
-    for (let i = 1; i <= 10; i++) {
-        if (i <= 3) {
-            backgroundColor.push('#00DD00');
-            borderColor.push('#00DD00');
-        } else {
-            backgroundColor.push('#979797');
-            borderColor.push('#97979752');
-        }
-    }
 
     const barData = {
         labels: labels,
@@ -28,8 +17,8 @@ function BarChart({ data, width = '100%', height = '100%' }) {
                 indexAxis: 'y',
                 data: values,
                 fill: false,
-                backgroundColor: backgroundColor,
-                borderColor: borderColor,
+                backgroundColor: BACKGROUNDCOLOR,
+                borderColor: BORDERCOLOR,
                 borderWidth: 1,
                 label: '단어 빈도수',
             },
